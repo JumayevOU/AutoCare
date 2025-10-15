12e3f0b05c03129d040f3d3e4f59d5b2f6b7f4b2
JumayevOU <jumayevou@gmail.com>
2025-10-15 08:01:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import asyncio
-from loader import dp, bot
-from handlers import setup_routers
+from aiogram import Bot, Dispatcher
+from database import init_db, close_db
 
 async def main():
-    setup_routers(dp)
-    await dp.start_polling(bot)
+    await init_db()
+    
+    bot = Bot(token=API_TOKEN)
+    dp = Dispatcher()
+    
+    
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await close_db()
 
 if __name__ == "__main__":
     asyncio.run(main())
