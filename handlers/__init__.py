69fbe2736d88ee5ee3f3aed3b0e61a3e8eced2ad
JumayevOU <jumayevou@gmail.com>
2025-10-15 09:33:09 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -8,3 +8,19 @@ def setup_routers(dp):
     users.setup(dp)
 
 
+# handlers/__init__.py
+from .users.menuHendlers import router as menu_router
+from .users.locations_hendler import router as locations_router
+from .users.start import router as start_router
+from .users.hamkorlik import router as hamkorlik_router
+from .users.echo import router as echo_router
+# from .users.admin_panel import router as admin_router  # Agar admin panel bo'lsa
+
+def setup_routers(dp):
+    """Routerlarni Dispatcher ga qo'shish"""
+    dp.include_router(start_router)
+    dp.include_router(menu_router)
+    dp.include_router(locations_router)
+    dp.include_router(hamkorlik_router)
+    dp.include_router(echo_router)
+    # dp.include_router(admin_router)  # Agar admin panel bo'lsa
