c74cb262542c27673c9b13059c2d6cedadbb0add
JumayevOU <jumayevou@gmail.com>
2025-10-15 08:56:12 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -8,20 +8,3 @@ def setup_routers(dp):
     users.setup(dp)
 
 
-from .users.menuHendlers import router as menu_router
-from .users.locations_hendler import router as locations_router
-from .users.start import router as start_router
-from .users.hamkorlik import router as hamkorlik_router
-from .users.echo import router as echo_router
-from .admin_panel import router as admin_router
-
-def setup_routers():
-    routers = [
-        start_router,
-        menu_router,
-        locations_router,
-        hamkorlik_router,
-        echo_router,
-        admin_router  
-    ]
-    return routers
